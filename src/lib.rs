714038ce4443545d64a4809540cdf0d25afdae63
beans15 <beans15@example.com>
2024-02-05 23:21:10 +0900

    Added limitation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+#[cfg(feature = "limit")]
+use std::sync::atomic::AtomicI8;
+
 use anyhow::Result;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
+
+#[cfg(feature = "limit")]
+static COUNTER: AtomicI8 = AtomicI8::new(0);
+
+#[cfg(feature = "limit")]
+static MAX_COUNT: i8 = 3;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
@@ -22,7 +31,32 @@ struct RequestParams<'a> {
     target: &'a str,
 }
 
+#[cfg(feature = "limit")]
 pub async fn translate(text: &str, source: &str, target: &str) -> Result<String> {
+    use std::{hint::spin_loop, sync::atomic::Ordering, time::Duration};
+
+    while let Err(_) = COUNTER.fetch_update(Ordering::Relaxed, Ordering::Acquire, |current| {
+        if current < MAX_COUNT {
+            Some(current + 1)
+        } else {
+            None
+        }
+    }) {
+        tokio::time::sleep(Duration::from_millis(0)).await;
+        spin_loop();
+    }
+
+    let result = translate_impl(text, source, target).await;
+    COUNTER.fetch_sub(1, Ordering::Release);
+    result
+}
+
+#[cfg(not(feature = "limit"))]
+pub async fn translate(text: &str, source: &str, target: &str) -> Result<String> {
+    translate_impl(text, source, target).await
+}
+
+async fn translate_impl(text: &str, source: &str, target: &str) -> Result<String> {
     let data = RequestParams {
         text,
         source,
